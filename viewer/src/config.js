27339b7a169cc31608e7779dc5218a2e7902b855
jhkim31 <biz.jh.kim@gmail.com>
2022-07-13 12:46:21 +0900

    Typescript 변환 실패 topology 모듈 변환

--- viewer/src/config.js
+++ viewer/src/config.js
@@ -1,6 +1,6 @@
 export const config = {
-    "base_url" : "http://172.20.10.14:5000/"    ,
-    // "base_url" : "http://localhost:5000", 
+    // "base_url" : "http://172.20.10.14:5000/"    ,
+    "base_url" : "http://localhost:5000", 
     "layout" : {
         "row" : "80px 400px 1fr",
         "col" : "200px 1fr",
